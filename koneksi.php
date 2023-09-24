dae3b8e8f0baa28d03d0397fd4cbe2742bb3611b
Rexxza <reksaarya0@gmail.com>
2023-09-24 16:08:23 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+    /*membuat variabel config berisi fungsi mysqli_connect
+    untuk menghubungkan ke database
+    host = localhost
+    user = root
+    pass = ""
+    database = bk-tamu
+    */
+    
+    $config=mysqli_connect("localhost","root","","bk-tamu");
+?>
